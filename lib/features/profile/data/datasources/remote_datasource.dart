b6c30655eb86a20946d4e3faf822cbe10a7fd158
syaokifaradisa09 <syaokifaradisa09@gmail.com>
2024-06-11 02:52:33 +0000

    refactor: preparation for unit testing

--- lib/features/profile/data/datasources/remote_datasource.dart
+++ lib/features/profile/data/datasources/remote_datasource.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:myapp/core/error/exception.dart';
 import 'package:myapp/features/profile/data/models/profile_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,10 +10,23 @@ abstract class ProfileRemoteDataSource{
 }
 
 class ProfileRemoteDataSourceImplementation extends ProfileRemoteDataSource{
+  final http.Client client;
+  ProfileRemoteDataSourceImplementation({
+    required this.client
+  });
+
   @override
   Future<ProfileModel> getUserById(int id) async{
     Uri url = Uri.parse("https://reqres.in/api/users/$id");
-    var response = await http.get(url);
+    var response = await client.get(url);
+
+    if (response.statusCode == 404) {
+      throw EmptyException(
+        message: "Data not found - Error 404"
+      );
+    }else if(response.statusCode != 200){
+      throw GeneralException(message: 'cannot get data');
+    }
 
     Map<String, dynamic> dataBody = jsonDecode(response.body);
     Map<String, dynamic> data = dataBody['data'];
@@ -23,10 +37,21 @@ class ProfileRemoteDataSourceImplementation extends ProfileRemoteDataSource{
   @override
   Future<List<ProfileModel>> getAllUser(int page) async {
     Uri url = Uri.parse("https://reqres.in/api/users?page=$page");
-    var response = await http.get(url);
+    var response = await client.get(url);
+
+    if (response.statusCode == 404) {
+      throw EmptyException(
+        message: "Data not found - Error 404"
+      );
+    }else if(response.statusCode != 200){
+      throw GeneralException(message: 'cannot get data');
+    }
 
     Map<String, dynamic> dataBody = jsonDecode(response.body);
     List<dynamic> data = dataBody['data'];
+    if(data.isEmpty){
+      throw EmptyException(message: 'Empty data - Error 404');
+    }
 
     return data.map((e) => ProfileModel.fromJson(e)).toList();
   }
